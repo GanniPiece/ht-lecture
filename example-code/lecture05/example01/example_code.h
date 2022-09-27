7a4a6eb5f59f3295a69c96e4b0b22e73c1831751
GanniPiece <p78101027@gs.ncku.edu.tw>
2022-09-27 14:21:02 +0800

    Auto saved by Logseq

--- example-code/lecture05/example01/example_code.h
+++ example-code/lecture05/example01/example_code.h
@@ -1,3 +1,7 @@
+#ifndef EXAMPLE_CODE_H
+#define EXAMPLE_CODE_H
+#include "example_header.h"
+#endif
 
 void myFunc1 (int);
 void myFunc2 (int);
--- example-code/lecture05/example01/example_header.h
+++ example-code/lecture05/example01/example_header.h
@@ -1,3 +1,3 @@
 
-extern const int m_var1 = 5;
+const int m_var1 = 5;
 const int m_var2 = 10;
