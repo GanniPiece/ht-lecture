a080aa86dc16c2b901612ba3c58841375f271b4d
GanniPiece <p78101027@gs.ncku.edu.tw>
2022-09-27 14:38:02 +0800

    Auto saved by Logseq

--- example-code/lecture05/example01/example_code.c
+++ example-code/lecture05/example01/example_code.c
@@ -1,5 +1,7 @@
-#pragma once
-#include "example_code.h"
+#ifndef EXAMPLE_CODE_H_
+    #define EXAMPLE_CODE_H_
+    #include "example_code.h"
+#endif
 
 void myFunc1 (int num) {
     printf("Func1: %d\n", num);
