a63d354649fefdc1f9a6e3c2e5453a27be2bc0a3
GanniPiece <p78101027@gs.ncku.edu.tw>
2022-09-27 14:35:02 +0800

    Auto saved by Logseq

--- example-code/lecture05/example01/example_header.c
+++ example-code/lecture05/example01/example_header.c
@@ -1,3 +1,11 @@
+#ifndef EXAMPLE_CODE_H_
+    #define EXAMPLE_CODE_H_
+    #include "example_code.h"
+#endif
+
+#ifndef EXAMPLE_HEADER_H_
+    #define EXAMPLE_HEADER_H_
+#endif
 
 const int m_var1 = 5;
 const int m_var2 = 10;
