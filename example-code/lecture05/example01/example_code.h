656b33da06d17d7c81dc5b68d9f0aa75fdea80c9
GanniPiece <p78101027@gs.ncku.edu.tw>
2022-09-27 14:13:02 +0800

    Auto saved by Logseq

--- example-code/lecture05/example01/example_code.h
+++ example-code/lecture05/example01/example_code.h
@@ -1,6 +1,7 @@
-#ifndef _EXAMPLE_HEADER_H
-    #include "example_header.h"
-#endif
+#ifndef EXAMPLE_HEADER_H_
+#define EXAMPLE_HEADER_H_
+
+#include "example_header.h"
 
 void myFunc1 (int);
 void myFunc2 (int);
