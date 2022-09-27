d666578f8ed57eb9cbdde696dc62ace317d9bdd0
GanniPiece <p78101027@gs.ncku.edu.tw>
2022-09-27 14:36:02 +0800

    Auto saved by Logseq

--- example-code/lecture05/example01/example_code.h
+++ example-code/lecture05/example01/example_code.h
@@ -1,6 +1,11 @@
-#pragma once
-#include "example_header.c"
-#include <stdio.h>
+#ifndef EXAMPLE_HEADER_H_
+    #define EXAMPLE_HEADER_H_
+    #include "example_header.c"
+#endif
+#ifndef STDIO_H_
+    #define STDIO_H_
+    #include <stdio.h>
+#endif
 
 void myFunc1 (int);
 void myFunc2 (int);
