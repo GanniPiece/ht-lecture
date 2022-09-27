f0b988583350e6661009c6ea9c0e43f0677af65f
GanniPiece <p78101027@gs.ncku.edu.tw>
2022-09-27 15:55:03 +0800

    Auto saved by Logseq

--- example-code/lecture05/example01/example_header.h
+++ example-code/lecture05/example01/example_header.h
@@ -1,5 +1,4 @@
-#ifndef EXAMPLE_HEADER_H_
-#define EXAMPLE_HEADER_H_
+#define EXAMPLE_HEADER_H
 
 int m_var1 = 5;
 int m_var2 = 10;
--- example-code/lecture05/example01/main.c
+++ example-code/lecture05/example01/main.c
@@ -2,11 +2,10 @@
 #include "example_header.h"
 #ifndef EXAMPLE_CODE_H_
 #include "example_code.h"
-
+#endif
 int main () {
     myFunc1(2);
     myFunc2(3);
 }
 
-#endif
 #endif
