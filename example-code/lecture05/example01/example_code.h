82c2bb728fda2556ab67e6cc744db43c392c9267
GanniPiece <p78101027@gs.ncku.edu.tw>
2022-09-27 14:48:02 +0800

    Auto saved by Logseq

--- example-code/lecture05/example01/example_code.h
+++ example-code/lecture05/example01/example_code.h
@@ -1,8 +1,8 @@
 #ifndef EXAMPLE_HEADER_H_
 #include "example_header.h"
 #ifndef STDIO_H_
-    #define STDIO_H_
-    #include <stdio.h>
+#define STDIO_H_
+#include <stdio.h>
 
 void myFunc1 (int);
 void myFunc2 (int);
