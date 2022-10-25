39d02ccd94dddb9654f472676363e15105dc137b
GanniPiece <p78101027@gs.ncku.edu.tw>
2022-10-25 23:14:43 +0800

    Auto saved by Logseq

--- example-code/lecture10/mutex_with_lock.py
+++ example-code/lecture10/mutex_with_lock.py
@@ -2,35 +2,39 @@ import threading
 
 var = 0
 
-def task_1():
+def task_1(lock):
     global var
     cnt = 3000
 
-    print("task 1")
-    print("1: ", var)
+    with lock:
+        print("task 1")
+        print("1: ", var)
 
-    while(cnt):
-        cnt = cnt - 1
+        while(cnt):
+            cnt = cnt - 1
 
-    var = var + 1
-    print("1: ", var)
+        var = var + 1
+        print("1: ", var)
 
-def task_2():
+def task_2(lock):
     global var
     cnt = 3000
 
-    print("task 2")
-    print("2: ", var)
+    with lock:
+        print("task 2")
+        print("2: ", var)
 
-    while(cnt):
-        cnt = cnt - 1
+        while(cnt):
+            cnt = cnt - 1
 
-    var = var + 2
-    print("2: ", var)
+        var = var + 2
+        print("2: ", var)
 
 if __name__ == "__main__":
-    t1 = threading.Thread(target=task_1)
-    t2 = threading.Thread(target=task_2)
+    lock = threading.Lock()
+
+    t1 = threading.Thread(target=task_1, args=(lock,))
+    t2 = threading.Thread(target=task_2, args=(lock,))
 
     t1.start()
     t2.start()
