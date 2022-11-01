eba5a14b56175aa4bb325fa8dbf1c65addd0d056
GanniPiece <p78101027@gs.ncku.edu.tw>
2022-11-01 16:16:09 +0800

    Auto saved by Logseq

--- example-code/call_by_value.cpp
+++ example-code/call_by_value.cpp
@@ -11,4 +11,12 @@ int add_ref (int &x, int &y) {
 }
 
 int main () {
+	int x = 1;
+	int y = 2;
+	std::cout << "add(val) x + y = " << add_val(x, y) << std::endl;
+	std::cout << "After add(val): x=" << x << " y=" << y << std::endl;
+	std::cout << "add(ref) x + y = " << add_ref(x, y) << std::endl;
+	std::cout << "After add(val): x=" << x << " y=" << y << std::endl;
+
+	return 0;
 }
