02e7c2f468306342dd1d585018e2e0bc9bf84f0d
GanniPiece <p78101027@gs.ncku.edu.tw>
2022-11-02 16:12:31 +0800

    Auto saved by Logseq

---
+++ example-code/lecture11/c_practice/int_manipulate.c
@@ -0,0 +1,17 @@
+#include "int_manipulate.h"
+
+int add (int a, int b) {
+    return 0;
+}
+
+int sub (int a, int b) {
+    return 0;
+}
+
+int mul (int a, int b) {
+    return 0;
+}
+
+int div (int a, int b) {
+    return 0;
+}
