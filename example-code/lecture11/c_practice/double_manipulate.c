d0b71796fe227e9c1b1eaba233c67ed0e135355c
GanniPiece <p78101027@gs.ncku.edu.tw>
2022-11-02 16:10:31 +0800

    Auto saved by Logseq

--- example-code/lecture11/c_practice/double_manipulate.c
+++ example-code/lecture11/c_practice/double_manipulate.c
@@ -1,5 +1,17 @@
 #include "double_manipulate.h"
 
 double add (double a, double b) {
-    
+    return 0.0;    
+}
+
+double sub (double a, double b) {
+    return 0.0;
+}
+
+double mul (double a, double b) {
+    return 0.0;
+}
+
+double div (double a, double b) {
+    return 0.0;
 }
