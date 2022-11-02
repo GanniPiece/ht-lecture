416bcdf6c5dd5e37f3d78c09ac988b75a09160aa
GanniPiece <p78101027@gs.ncku.edu.tw>
2022-11-02 17:02:31 +0800

    Auto saved by Logseq

--- example-code/lecture11/c_practice/double_manipulate.c
+++ example-code/lecture11/c_practice/double_manipulate.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "double_manipulate.h"
 
-double double_add (void *a, void *b) {
+double* double_add (void *a, void *b) {
     printf("%lf %lf\n", *(double*)a, *(double*) b);
-    return *((double*) a) + *((double*) b);    
+    double ans = *(double*) a + *(double*) b;    
+    return ans;
 }
 
 double double_sub (double *a, double *b) {
