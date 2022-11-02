210f7c9eb7b978440d53911e18029ae7bb4d0d2d
GanniPiece <p78101027@gs.ncku.edu.tw>
2022-11-02 17:08:31 +0800

    Auto saved by Logseq

--- example-code/lecture11/c_practice/double_manipulate.c
+++ example-code/lecture11/c_practice/double_manipulate.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 #include "double_manipulate.h"
 
-void double_add (void *ans, void *a, void *b) {    
+void double_add (double *ans, double *a, double *b) {    
 }
 
-void double_sub (void *ans, void *a, void *b)  {
-    return 0.0;
+void double_sub (double *ans, double *a, double *b)  {
 }
 
-void double_mul (void *ans, void *a, void *b)  {
-    return 0.0;
+void double_mul (double *ans, double *a, double *b)  {
 }
 
-void double_div (void *ans, void *a, void *b)  {
-    return 0.0;
+void double_div (double *ans, double *a, double *b)  {
 }
--- example-code/lecture11/c_practice/int_manipulate.c
+++ example-code/lecture11/c_practice/int_manipulate.c
@@ -5,13 +5,10 @@ void int_add (int* ans, int *a, int *b) {
 }
 
 void int_sub (int* ans, int *a, int *b) {
-    return 0;
 }
 
 void int_mul (int* ans, int *a, int *b) {
-    return 0;
 }
 
 void int_div (int* ans, int *a, int *b) {
-    return 0;
 }
