71fe9e6f4fd26e4601016eba9099cf2089166f46
GanniPiece <p78101027@gs.ncku.edu.tw>
2022-11-02 17:35:31 +0800

    Auto saved by Logseq

--- example-code/lecture11/c_practice/main.c
+++ example-code/lecture11/c_practice/main.c
@@ -3,16 +3,11 @@
 
 int main () {
 
-
+    int a, b, ans_int;
     void (*manipulate_int)(int*, int*, int*);
+    
     manipulate_int = &add_int;
+    manipulate_int(&ans_int, &a, &b);
+    printf("%d\n", ans_int);
 
-        printf("%d\n", ans_int);
-
-    
-    
-    double c = 8.3, d = 9.1, ans_double;
-    add = &double_add;
-    add(&ans_double, &c, &d);
-    printf("%lf\n", ans_double);
 }
