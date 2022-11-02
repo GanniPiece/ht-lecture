579f8b1b0b85f389eb6e1e825db52d14148c150b
GanniPiece <p78101027@gs.ncku.edu.tw>
2022-11-02 17:37:31 +0800

    Auto saved by Logseq

--- example-code/lecture11/c_practice/main.c
+++ example-code/lecture11/c_practice/main.c
@@ -6,19 +6,21 @@ int main () {
     int a=5, b=2, ans_int;
     void (*manipulate_int)(int*, int*, int*);
     
+    printf("a = %d, b = %d\n", a, b);
+
     manipulate_int = &int_add;
     manipulate_int(&ans_int, &a, &b);
-    printf("%d\n", ans_int);
+    printf("add: %d\n", ans_int);
 
     manipulate_int = &int_sub;
     manipulate_int(&ans_int, &a, &b);
-    printf("%d\n", ans_int);
+    printf("sub: %d\n", ans_int);
 
     manipulate_int = &int_mul;
     manipulate_int(&ans_int, &a, &b);
-    printf("%d\n", ans_int);
+    printf("mul: %d\n", ans_int);
 
     manipulate_int = &int_div;
     manipulate_int(&ans_int, &a, &b);
-    printf("%d\n", ans_int);
+    printf("div: %d\n", ans_int);
 }
