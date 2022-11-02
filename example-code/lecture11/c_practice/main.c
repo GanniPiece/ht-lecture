1e3e47a4d88383584a03dc1743b4d7537a890a38
GanniPiece <p78101027@gs.ncku.edu.tw>
2022-11-02 17:34:31 +0800

    Auto saved by Logseq

--- example-code/lecture11/c_practice/main.c
+++ example-code/lecture11/c_practice/main.c
@@ -2,16 +2,14 @@
 #include "calculator.h"
 
 int main () {
-    void (*int_candidate) (int*,int*,int*) = {&int_add, &int_sub, &int_mul, &int_div};
-    void (*manipulate_int) (int*, int*, int*);
 
-    int a = 5, b = 3, ans_int;
-    for (int i = 0; i < 4; i++) {
-        manipulate_int = int_candidate[i];
-        add(&ans_int, &a, &b);
+
+    void (*manipulate_int)(int*, int*, int*);
+    manipulate_int = &add_int;
+
         printf("%d\n", ans_int);
 
-    }
+    
     
     double c = 8.3, d = 9.1, ans_double;
     add = &double_add;
