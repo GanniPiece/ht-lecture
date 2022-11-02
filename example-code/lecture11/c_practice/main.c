93765a6497772a60f97d095babb36542f7f3b49e
GanniPiece <p78101027@gs.ncku.edu.tw>
2022-11-02 16:52:31 +0800

    Auto saved by Logseq

--- example-code/lecture11/c_practice/main.c
+++ example-code/lecture11/c_practice/main.c
@@ -1,6 +1,7 @@
+#include <stdio.h>
 #include "calculator.h"
 
 int main () {
     int a = 5, b = 3;
-    printf("%d\n", (int*) add((void*) &a, (void*) &b));
+    printf("%d\n", *((int*) add((void*) &a, (void*) &b)));
 }
