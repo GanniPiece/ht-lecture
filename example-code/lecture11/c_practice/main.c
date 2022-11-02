36dd5a2d0accedfac2282e9d11f5e7a1b69806d7
GanniPiece <p78101027@gs.ncku.edu.tw>
2022-11-02 17:10:31 +0800

    Auto saved by Logseq

--- example-code/lecture11/c_practice/main.c
+++ example-code/lecture11/c_practice/main.c
@@ -2,11 +2,13 @@
 #include "calculator.h"
 
 int main () {
-    int a = 5, b = 3;
+    int a = 5, b = 3, ans_int;
     add = &int_add;
-    printf("%d\n", (int) add(&a, &b));
+    add(&ans_int, &a, &b)
+    printf("%d\n", ans_int);
 
-    double c = 8.3, d = 9.1;
+    double c = 8.3, d = 9.1, ans_double;
     add = &double_add;
-    printf("%lf\n", add(&c, &d));
+    add(&ans_double, &c, &d)
+    printf("%lf\n", );
 }
